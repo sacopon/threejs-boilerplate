b1b55cf6f7127c3e771c2176f86ca1205555a245
Takeshi Sakurai <sacopon.dev@gmail.com>
2020-04-08 09:40:29 +0900

    initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPluginOptions = {
+  template: "src/index.html.ejs",
+  inject: "body",
+  hash: true,
+};
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      // for .ts files
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(htmlWebpackPluginOptions),
+  ],
+  devServer: {
+    host: "0.0.0.0",
+    port: 8080,
+  }
+};
